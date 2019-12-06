a7dc3059f22357215376a78bceaa3192c32c2e20
Daniel Matney <danieljmatney@gmail.com>
2019-12-06 06:19:42 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,4 +18,5 @@ class Pokemon
     pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", [id])
     new_pokemon = Pokemon.new(id: pokemon[0],name: pokemon[1],type: pokemon[2],db: db)
   end
+  
 end
